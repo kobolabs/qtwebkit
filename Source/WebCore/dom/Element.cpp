6654ea5e6d8225e604fa64febe3b800ec751066b
Moiez Ul Hasnain <moiez.hasnain@rakuten.com>
2016-01-19 09:25:09 +0900

    Merged proposed patch from Webkit bug 131842 for Webkit css animation

--- Source/WebCore/dom/Element.cpp
+++ Source/WebCore/dom/Element.cpp
@@ -1584,7 +1584,7 @@ void Element::recalcStyle(StyleChange change)
         }
 
         if (RenderObject* renderer = this->renderer()) {
-            if (localChange != NoChange || pseudoStyleCacheIsInvalid(currentStyle.get(), newStyle.get()) || (change == Force && renderer->requiresForcedStyleRecalcPropagation()) || styleChangeType() == SyntheticStyleChange)
+            if (!currentStyle.get()->maxHeight().isUndefined() && (localChange != NoChange || pseudoStyleCacheIsInvalid(currentStyle.get(), newStyle.get()) || (change == Force && renderer->requiresForcedStyleRecalcPropagation()) || styleChangeType() == SyntheticStyleChange))
                 renderer->setAnimatableStyle(newStyle.get());
             else if (needsStyleRecalc()) {
                 // Although no change occurred, we use the new style so that the cousin style sharing code won't get
